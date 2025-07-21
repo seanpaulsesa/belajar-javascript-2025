ca0429367f1091fb3e24273e249ff47c45511101
Paulus sesa <114337187+seanpaulsesa@users.noreply.github.com>
2025-07-21 10:06:56 -0700

    added menambahkan file project

---
+++ main.js
@@ -0,0 +1,20 @@
+document.title = 'paulus sesa'
+const btn1 = document.getElementById('btn1')
+const btn2 = document.getElementById('btn2')
+const btn3 = document.getElementById('btn3')
+const btn4 = document.getElementById('btn4')
+
+
+btn1.style.border = 'none'
+btn1.style.padding = '8px'
+btn1.style.fontSize = '24px'
+btn1.style.background = 'tomato'
+
+
+
+
+
+// console.log(btn1)
+// console.log(btn2)
+// console.log(btn3)
+// console.log(btn4)
